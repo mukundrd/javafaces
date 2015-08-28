5a62fccf3dbd3fea58c34cc0ef20a4c10505d7f6
Mukund Desai <mukund.desai@honeywell.com>
2015-08-28 12:18:24 +0530

    adding source

---
+++ code/javafaces/src/main/java/facerecognition/javafaces/FaceRecError.java
@@ -0,0 +1,11 @@
+
+package facerecognition.javafaces;
+
+public class FaceRecError extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public FaceRecError(String msg) {
+        super(msg);
+    }
+}
